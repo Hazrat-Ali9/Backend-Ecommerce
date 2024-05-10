b4e6fd067a976d262303bae60502dad672dfad67
Hazrat-Ali9 <122374893+Hazrat-Ali9@users.noreply.github.com>
2024-05-10 23:04:13 +0600

    :boom:  Post

--- server.js
+++ server.js
@@ -75,7 +75,7 @@ async function run() {
       const users = await productCollection.find(query).toArray();
       res.send(users);
     });
-    
+    // post 
     app.post("/product", upload.single('image'), async (req, res) => {
       try {
         const status = req.body.status || 'pending';
